dd1635ef2c73f1e33b3ac09b78e93be978eadecc
NorteX <contact@nortexdev.com>
2024-08-03 00:20:52 +0200

    simplify import

--- tests/migrator.test.ts
+++ tests/migrator.test.ts
@@ -2,7 +2,7 @@ import { expect, test, beforeEach, afterEach } from "bun:test";
 import { readFileSync, writeFileSync, unlinkSync, copyFileSync } from "fs";
 import * as path from "path";
 import * as yaml from "js-yaml";
-import { type Config, Migration } from "../src/migrate.ts";
+import { type Config, Migration } from "../src";
 
 // Test suite
 const testConfigPath = "./test-config.yml";
